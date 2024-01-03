3a4b41679adceb598b0d8c54a7321a6b2e145c0c
Aaron Friedman <afriedman@yext.com>
2024-01-03 15:41:22 -0500

    add deploy and site id from site arg

--- src/functions/http/preview/[templateId]/entity/[entityId].ts
+++ src/functions/http/preview/[templateId]/entity/[entityId].ts
@@ -11,11 +11,11 @@ export default async function generatePreview(
   const templateId = pathParams['templateId']
   const entityId = pathParams['entityId']
 
-  const entityResponse = await fetch(`https://sbx-api.yextapis.com/v2/accounts/me/sites/159685/fetchentitydocument?entityId=${entityId}&templateId=${templateId}&api_key=1180012a82bbd5acd01414744612c3df&v=20230601`);
+  const entityResponse = await fetch(`https://sbx-api.yextapis.com/v2/accounts/me/sites/${site.siteId}/fetchentitydocument?entityId=${entityId}&templateId=${templateId}&deploymentId=${site.deployId}&api_key=1180012a82bbd5acd01414744612c3df&v=20230601`);
   const entityJson = await entityResponse.json();
   const entityData = entityJson.response.document
 
-  const generateResponse = await fetch(`https://sbx-api.yextapis.com/v2/accounts/me/sites/159685/generatepagecontent?templateId=${templateId}&api_key=1180012a82bbd5acd01414744612c3df&v=20161012`, 
+  const generateResponse = await fetch(`https://sbx-api.yextapis.com/v2/accounts/me/sites/${site.siteId}/generatepagecontent?templateId=${templateId}&deploymentId=${site.deployId}&api_key=1180012a82bbd5acd01414744612c3df&v=20161012`, 
     {method: 'POST', body: JSON.stringify(entityData), headers: {'Content-Type': 'application/json'}});
   const generateJson = await generateResponse.json();
 
