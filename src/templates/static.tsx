885a67111c9097184dec170b3437281400c0bdc8
dlsgusrn7577 <dlee@yext.com>
2024-04-08 14:38:04 -0400

    Update static.tsx

--- src/templates/static.tsx
+++ src/templates/static.tsx
@@ -125,7 +125,7 @@ export const config: TemplateConfig = {
     ],
     // Defines the scope of entities that qualify for this stream.
     filter: {
-      entityIds: [import.meta.env.YEXT_PUBLIC_ENTITY_ID],
+      entityIds: ["YEXT_PUBLIC_ENTITY_ID"],
       // entityTypes: ['location'],
     },
     // The entity language profiles that documents will be generated for.
