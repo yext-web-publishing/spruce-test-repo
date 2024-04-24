fba50e50e100d0b451290e68e403772f513f31a0
Jacob Wartofsky <jwartofsky@yext.com>
2024-04-24 16:11:38 -0400

    Added closed-locations.tsx

---
+++ src/redirects/closed-locations.tsx
@@ -0,0 +1,43 @@
+import {
+  RedirectConfig,
+  TemplateProps,
+  GetDestination,
+  GetSources,
+} from "@yext/pages";
+
+export const config: RedirectConfig = {
+  stream: {
+    $id: "closed-location-redirects",
+    fields: ["id", "address", "name"],
+    filter: {
+      entityTypes: ["location"],
+    },
+    localization: {
+      locales: ["en"]
+    }
+  },
+};
+
+/**
+ * Defines the URL to redirect the source paths to.
+ */
+export const getDestination: GetDestination<TemplateProps> = ({ document }) => {
+  return document.id;
+};
+
+
+/**
+ * Defines a list of redirect source objects, which will redirect to the URL created by getDestination.
+ */
+export const getSources: GetSources<TemplateProps> = ({ document }) => {
+  return [
+    {
+      "source": `closed/${document.locale}/${document.id.toString()}`,
+      "status": 301
+    },
+    {
+      "source": `closed302/${document.locale}/${document.id.toString()}`,
+      "status": 302
+    },
+  ];
+};
