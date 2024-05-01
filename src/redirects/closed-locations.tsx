f7a7f5bd18a3c353ae2ebdd33ea5926d9f90ee16
jhelenek <141873056+jhelenek@users.noreply.github.com>
2024-05-01 15:07:04 -0400

    Update closed-locations.tsx

--- src/redirects/closed-locations.tsx
+++ src/redirects/closed-locations.tsx
@@ -10,7 +10,7 @@ export const config: RedirectConfig = {
     $id: "closed-location-redirects",
     fields: ["id", "address", "name"],
     filter: {
-      entityIds: ["location5"],
+      entityIds: ["8353926"],
     },
     localization: {
       locales: ["en"]
