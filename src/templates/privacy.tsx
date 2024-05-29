74193965602d1d254417f62277dc9174476825b1
dlsgusrn7577 <dlee@yext.com>
2024-05-29 16:23:00 -0400

    Update privacy.tsx

--- src/templates/privacy.tsx
+++ src/templates/privacy.tsx
@@ -29,7 +29,7 @@ import {
  * This allows the user to define a function which will take in their template
  * data and procude a HeadConfig object. When the site is generated, the HeadConfig
  * will be used to generate the inner contents of the HTML document's <head> tag.
- * This can include the title, meta tags, script tags, etc.
+ * This can include the title, meta tags, script tags, etc..
  */
 export const getHeadConfig: GetHeadConfig<
   TemplateRenderProps
