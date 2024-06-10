f04dec862f3e08b83b5bb7df732be854c0456494
Aaron Friedman <afriedman@yext.com>
2024-06-10 09:57:47 -0400

    remove local files

--- .artifact-output/desktop/assets/static/yext-favicon-nOZ6gOzL.js
+++
@@ -1,1 +0,0 @@
-const a="/assets/static/yext-favicon-KeWL9RsD.ico";export{a as F};
--- dist/assets/render/_client.TFfooV3y.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"../static/index-d2pGKYpz.js";import{r as a}from"../static/index-OwpHvwnT.js";import"../static/commonjsHelpers-DxDlwT5B.js";const p=async e=>{const{Page:r,pageProps:t}=e;o.hydrate(a.createElement(r,{...t}),document.getElementById("reactele"))};export{p as render};
