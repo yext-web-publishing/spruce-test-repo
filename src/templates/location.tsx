bfe29ae65604333b39e45cc1ec685440129cc48a
yqiu-yext <yqiu@yext.com>
2024-06-21 14:03:39 -0400

    added filter id

--- src/templates/location.tsx
+++ src/templates/location.tsx
@@ -41,6 +41,7 @@ export const config: TemplateConfig = {
     // You can use entityTypes, savedFilterIds, and/or entityIds
     filter: {
       entityTypes: ["location"],
+      savedFilterIds: ["1389778186"],
     },
     // Specifies the exact data that each generated document will contain.
     // This data is passed in directly as props to the default exported function.
