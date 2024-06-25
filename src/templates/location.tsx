8407f98d33e352526fba84c7f5268ed971ea218e
yqiu-yext <yqiu@yext.com>
2024-06-25 11:52:47 -0400

    added temporal component date

--- src/templates/location.tsx
+++ src/templates/location.tsx
@@ -182,6 +182,7 @@ const Location: Template<TemplateRenderProps> = ({
             <Details address={address} phone={mainPhone} services={services} />
             {hours && <Hours title={"Restaurant Hours"} hours={hours} />}
             {description && <About name={name} description={description} />}
+            {(new Date()).toISOString()}
           </div>
         </div>
       </PageLayout>
